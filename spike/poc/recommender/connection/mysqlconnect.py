d63caa36eee9c0491275eb4822ce2d5a812f4757
sahathat <61311677+sahathat@users.noreply.github.com>
2024-02-13 15:48:44 +0700

    version 3.3
    ML Recommender system
    - add exception handling of connection database

--- spike/poc/recommender/connection/mysqlconnect.py
+++ spike/poc/recommender/connection/mysqlconnect.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from dotenv import load_dotenv
 import os
+from fastapi import HTTPException, status
 
 # Load environment variables
 load_dotenv()
@@ -12,9 +13,18 @@ MYSQL_DB = os.getenv("MYSQL_DB")
 
 # Connect to MySQL
 def connect():
-    return mysql.connector.connect(
-        host=MYSQL_HOST,
-        user=MYSQL_USER,
-        password=MYSQL_PASSWORD,
-        database=MYSQL_DB
-    )
+    try:
+        cnx = mysql.connector.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database=MYSQL_DB
+        )
+        print("Connected to MySQL database successfully")
+        return cnx
+    except mysql.connector.Error as err:
+        print("Error:", err)
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Database error: " + str(err),
+        )
